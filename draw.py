bdf481dc0bb37bf229bf8d111e8fa3b44283278d
thementor1 <safa.c.nizamoglu@gmail.com>
2023-06-08 13:02:38 -0500

    NewVersion

--- draw.py
+++ draw.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
 import time
 
+browser = webdriver.Firefox()
+
+browser.get("https://avivawholesale.com/")
+html = browser.page_source
+time.sleep(5)
+print(html)
+
+browser.close()
+
