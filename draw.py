2f695ee114832729d248d5095f6d53384b0406da
thementor1 <safa.c.nizamoglu@gmail.com>
2023-06-08 13:35:39 -0500

    New_Versiont

--- draw.py
+++ draw.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import time
+
+instagramUrl = "https://www.youtube.com/watch?v=zrCLRC3Ci1c"
+
 
 browser = webdriver.Firefox()
 
-browser.get("https://avivawholesale.com/")
+browser.get(instagramUrl)
 html = browser.page_source
-time.sleep(5)
-print(html)
+time.sleep(10)
+
+soup = BeautifulSoup(html, 'html.parser')
+messages = soup.find_all("style-scope ytd-comment-renderer")
+
+for message in messages:
+    print(message)
 
 browser.close()
 
