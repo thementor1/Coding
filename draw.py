bafc35ec8df11e1a152b95da14dca23964a43aef
thementor1 <safa.c.nizamoglu@gmail.com>
2023-06-23 09:50:56 -0500

    commit changes

--- draw.py
+++ draw.py
@@ -2,17 +2,21 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-instagramUrl = "https://www.youtube.com/watch?v=zrCLRC3Ci1c"
+ytLiveChatUrl = "https://www.youtube.com/live_chat?v=mxTiT2igK7A"
 
 
 browser = webdriver.Firefox()
 
-browser.get(instagramUrl)
+browser.get(ytLiveChatUrl)
 html = browser.page_source
-time.sleep(15)
+time.sleep(10)
 
 soup = BeautifulSoup(html, 'html.parser')
 messages = soup.find_all("style-scope ytd-comment-renderer")
+
+for message in messages:
+    print(message)
+
 
 for message in messages:
     print(message)
