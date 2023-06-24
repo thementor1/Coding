83a079ec0ff7330df0bfec6c3d81fb39ec596201
thementor1 <safa.c.nizamoglu@gmail.com>
2023-06-24 18:20:40 -0500

    Age restriction is added

--- CS50P/hello.py
+++ CS50P/hello.py
@@ -1,5 +1,11 @@
 #Ask users name
-name = input("What is your name?")
+name = input("What is your name? ")
+
+#Ask users age
+age = int(input("What is your age? "))
 
 #Say hello to user
-print("Hello", (name))
+if age >= 18:
+    print("Hello and welcome", name)
+else:
+    print ("Hello", name, "you are so young to log in!")
