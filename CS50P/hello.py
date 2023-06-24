e191e67ae780e567d268790c0512d318705c8791
thementor1 <safa.c.nizamoglu@gmail.com>
2023-06-24 18:42:57 -0500

    c

--- CS50P/hello.py
+++ CS50P/hello.py
@@ -1,11 +1,6 @@
+import time 
 #Ask users name
 name = input("What is your name? ")
 
-#Ask users age
-age = int(input("What is your age? "))
-
 #Say hello to user
-if age >= 18:
-    print("Hello and welcome", name)
-else:
-    print ("Hello", name, "you are so young to log in!")
+print(f"Hello and welcome, {name}")
